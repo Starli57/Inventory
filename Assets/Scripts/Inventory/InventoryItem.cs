4b295e004478ccb0a4796acc3a230412f3e90839
Alexander Termer <termeralexandr@gmail.com>
2018-07-01 20:14:37 +0300

    Added items data

---
+++ Assets/Scripts/Inventory/InventoryItem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class InventoryItem
+{
+    public enum ItemsType { weapon = 0, armor = 1}
+
+    public int id;
+
+    public ItemsType itemType;
+
+    public int attack;
+    public int def;
+
+    public Sprite sprite;
+}
